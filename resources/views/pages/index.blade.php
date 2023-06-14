c4040f0bfdbe263ea77e1b88527d3daa6a939483
SimoneScordato99 <scordato.simone@yahoo.com>
2023-06-14 19:18:56 +0200

    aggiiustamenti

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,13 +3,12 @@
 @section('mario')
 <main>
     @foreach($comics as $elem)
-        <a href="/comics/{{$elem['id']}}">
+        <a href="/peppe/{{$elem['id']}}">
             <div class="card" style="width: 18rem;">
                 <img src="{{$elem['thumb']}}" class="card-img-top" alt="{{$elem['title']}}">
                 <div class="card-body">
                     <h5 class="card-title">{{$elem['title']}}</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <p>{{$elem['price']}}</p>
                 </div>
             </div>
         </a>
